618991a3e430f0bd1ad2f72c746656f3799dad7d
ItoKengo <qianwuyiteng851@gmail.com>
2025-01-11 21:27:53 +0900

    幅優先探索の土台の作成
    幅優先探索の土台を作成しました。
    playerや敵の座標からどうやって迷路情報が格納されている2次元配列にplayerや敵の位置情報を適用するのかが課題です。

--- Assets/Script/DepthFirstSearch.cs
+++ Assets/Script/DepthFirstSearch.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class DepthFirstSearch : MonoBehaviour
 {
+    MazeManager mazeManager;
+
+    int[,] maze;
+
+    int[,] visitedArray;
+
+    int mazeWidth;
+
+    int mazeHeight;
+
+    Vector2Int PlayerGridPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mazeManager = GetComponent<MazeManager>();
+        maze = mazeManager.GetMaze();
+        mazeWidth = maze.GetLength(0);
+        mazeHeight= maze.GetLength(1);
+        visitedArray = new int[mazeWidth , mazeHeight];
     }
 
     // Update is called once per frame
